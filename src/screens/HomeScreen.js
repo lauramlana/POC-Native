490b056c3563682ecec28ded5558353c718935e2
lauramlana <laura.lana@lexartlabs.xyz>
2023-11-06 22:24:29 -0300

    feat: add login with google working

--- src/screens/HomeScreen.js
+++ src/screens/HomeScreen.js
@@ -1,42 +1,62 @@
-import React from 'react';
-import { Text, View,  StyleSheet, Button, TouchableOpacity } from 'react-native';
-import { FIREBASE_AUTH } from '../../FirebaseConfig';
+import React, { useEffect, useState } from "react";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  TouchableOpacity,
+  Image,
+} from "react-native";
+import { FIREBASE_AUTH } from "../../FirebaseConfig";
 
- const HomeScreen = (props) => {
-    return (
-        <View>
-            <Text style={styles.text}>Home View</Text>
-            <Button 
-            onPress={() => props.navigation.navigate('Form')}
-            title='Go to Form View'
-            />
-            <TouchableOpacity
-            onPress={() => props.navigation.navigate('List')}
-            >
-                <Text style={styles.btnTouchable}>GO TO LIST VIEW</Text>
-            </TouchableOpacity>
-            <Button
-            title='Logout'
-            onPress={() => FIREBASE_AUTH.signOut()}
-            />
-        </View>  
-    )
-}
+const HomeScreen = (props) => {
+  const [user, setUser] = useState(null);
+
+  useEffect(() => {
+    const authUser = FIREBASE_AUTH.currentUser;
+    if (authUser) {
+      setUser(authUser);
+    }
+  }, []);
+
+  return (
+    <View>
+      <Text style={styles.text}>Home View</Text>
+      {user ? (
+        <>
+          <Text>Usuário logado: {user.displayName}</Text>
+          <Image
+            source={{ uri: user.photoURL }}
+            style={{ height: 200, width: 200, borderRadius: 150, marginHorizontal: 100, marginVertical:40 }}
+          />
+        </>
+      ) : null}
+      <Button
+        onPress={() => props.navigation.navigate("Form")}
+        title="Go to Form View"
+      />
+      <TouchableOpacity onPress={() => props.navigation.navigate("List")}>
+        <Text style={styles.btnTouchable}>GO TO LIST VIEW</Text>
+      </TouchableOpacity>
+      <Button title="Logout" onPress={() => FIREBASE_AUTH.signOut()} />
+    </View>
+  );
+};
 
 const styles = StyleSheet.create({
-    text: {
-        fontSize: 25,
-        marginVertical: 20,
-        marginHorizontal: 100
-      },
-      btnTouchable: {
-        fontSize: 15,
-        marginVertical: 10,
-        marginHorizontal: 120,
-        color: 'white',
-        fontWeight: 'bold',
-        backgroundColor: 'gray',
-      } 
-})
+  text: {
+    fontSize: 25,
+    marginVertical: 20,
+    marginHorizontal: 100,
+  },
+  btnTouchable: {
+    fontSize: 15,
+    marginVertical: 10,
+    marginHorizontal: 120,
+    color: "white",
+    fontWeight: "bold",
+    backgroundColor: "gray",
+  },
+});
 
 export default HomeScreen;
--- src/screens/LoginScreen.js
+++ src/screens/LoginScreen.js
@@ -1,14 +1,37 @@
 import React, { useState } from "react";
-import { Text, View, StyleSheet, Button, TextInput, KeyboardAvoidingView } from "react-native";
-import { FIREBASE_AUTH } from "../../FirebaseConfig";
-import { createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
+import {
+  Text,
+  View,
+  StyleSheet,
+  Button,
+  TextInput,
+  KeyboardAvoidingView,
+} from "react-native";
+import { FIREBASE_AUTH, FIREBASE_APP } from "../../FirebaseConfig";
+import {
+  createUserWithEmailAndPassword,
+  signInWithEmailAndPassword,
+  GoogleAuthProvider,
+  getAuth, 
+  signInWithCredential,
+} from "firebase/auth";
 import { ActivityIndicator } from "react-native";
+import {
+  GoogleSignin,
+  GoogleSigninButton,
+} from "@react-native-google-signin/google-signin";
 
-const LoginScreen = (props) => {
+const LoginScreen = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
   const [loading, setLoading] = useState(false);
+
   const auth = FIREBASE_AUTH;
+
+  GoogleSignin.configure({
+    webClientId:
+      "93265877309-7l4ncb4acfmdp0qcf73vsv18uqp8te5s.apps.googleusercontent.com",
+  });
 
   // const signUp = async () => {
   //   setLoading(true);
@@ -37,36 +60,67 @@ const LoginScreen = (props) => {
     }
   };
 
+  const handleGoogleSignIn = async () => {
+    // const { idToken } = await GoogleSignin.signIn();
+    // const googleCredential = GoogleAuthProvider.credential(idToken);
+    // console.log(googleCredential); // undefined
+    // const user_sign_in = auth().signInWithCredential(googleCredential);
+    // user_sign_in
+    //   .then((user) => {
+    //     console.log(user.familyName);
+    //   })
+    //   .catch((error) => {
+    //     console.log(error);
+    //   });
+
+    try {
+      await GoogleSignin.hasPlayServices();
+      const userInfo = await GoogleSignin.signIn();
+  
+      const { idToken, accessToken } = userInfo;
+  
+      const auth = getAuth(FIREBASE_APP); // Observe a mudança aqui
+      const googleCredential = GoogleAuthProvider.credential(idToken, accessToken);
+  
+      await signInWithCredential(auth, googleCredential);
+  
+      console.log('Usuário autenticado com sucesso:', auth.currentUser.displayName);
+    } catch (error) {
+      console.error('Erro ao fazer login com o Google:', error);
+    }
+  };
+
   return (
     <View>
       <Text style={styles.text}>Login View</Text>
       <KeyboardAvoidingView behavior="padding">
-      <Text style={styles.label}>Email:</Text>
-      <TextInput
-        value={email}
-        style={styles.input}
-        autoCapitalize="none"
-        onChangeText={(text) => setEmail(text)}
-      />
-      <Text style={styles.label}>Senha:</Text>
-      <TextInput
-        value={password}
-        style={styles.input}
-        autoCapitalize="none"
-        secureTextEntry={true}
-        onChangeText={(text) => setPassword(text)}
-      />
-      {loading ? (
-        <ActivityIndicator size="large" color="#0000ff" />
-      ) : (
-        <View>
-        <Button
-        onPress={signIn}
-          title="Login"
+        <Text style={styles.label}>Email:</Text>
+        <TextInput
+          value={email}
+          style={styles.input}
+          autoCapitalize="none"
+          onChangeText={(text) => setEmail(text)}
         />
-        {/* <Button onPress={signUp} title="Register" /> */}
-        </View>
-      )}
+        <Text style={styles.label}>Senha:</Text>
+        <TextInput
+          value={password}
+          style={styles.input}
+          autoCapitalize="none"
+          secureTextEntry={true}
+          onChangeText={(text) => setPassword(text)}
+        />
+        {loading ? (
+          <ActivityIndicator size="large" color="#0000ff" />
+        ) : (
+          <View>
+            <Button onPress={signIn} title="Login" />
+            {/* <Button onPress={signUp} title="Register" /> */}
+            <GoogleSigninButton
+              title="Google Sign-In"
+              onPress={handleGoogleSignIn}
+            />
+          </View>
+        )}
       </KeyboardAvoidingView>
     </View>
   );
